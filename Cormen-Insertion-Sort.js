f14a1b1e8e33a2792985633e948afad45d990187
Paabrown <paabrown@gmail.com>
2017-04-03 23:57:44 -0400

    Fixed naming convention for array values

--- Cormen-Insertion-Sort.js
+++ Cormen-Insertion-Sort.js
@@ -1,10 +1,10 @@
 function cormenInsertionSort(array) {
 	for (let i = 1; i < array.length; i++) {
-		let key = array[i]
+		let element = array[i]
 		let j = i - 1
-		while (j >= 0 && key < array[j]) {
+		while (j >= 0 && element < array[j]) {
 			array[j + 1] = array[j]
 			j--
-		} key = array[j + 1]
+		} element = array[j + 1]
 	} return array
 }
